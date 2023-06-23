cd859f29bc033c528270e2000b076cef572f2a73
Kathryn Groenewald <groenewk@sheridancollege.ca>
2023-06-23 13:04:30 -0400

    addded kathryn.java class

--- src/midtermexam_versiona_extensioncode/kathryn.java
+++ src/midtermexam_versiona_extensioncode/kathryn.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package java.util;
+package midtermexam_versiona_extensioncode;
 
 /**
  *
  * @author kathr
  */
-public class scanner {
-    
+public class kathryn {
+    //midterm2partb
+    //adding comments here
+    //the date is 2023/06/23 
 }
